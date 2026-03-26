61e5fc5af6fd46ded2a127dfdd451af53c039d48
dwells66 <dwells66@students.claremontmckenna.edu>
2026-03-26 00:43:25 -0700

    4

--- sql/04.sql
+++ sql/04.sql
@@ -7,19 +7,21 @@
 SELECT
     a.first_name,
     a.last_name
-    FROM actor a
-INNER JOIN
+FROM actor a
+JOIN
     (SELECT DISTINCT fa2.actor_id
         FROM film_actor AS fa2
         JOIN film_category AS fc2 ON fa2.film_id = fc2.film_id
         JOIN category AS c2 ON fc2.category_id = c2.category_id
-        WHERE c2.name = 'Children') AS sub1 ON a.actor_id = sub1.actor_id
+        WHERE c2.name = 'Children'
+) AS sub1 ON a.actor_id = sub1.actor_id
 LEFT JOIN
     (SELECT DISTINCT fa3.actor_id
         FROM film_actor fa3
         JOIN film_category AS fc3 ON fa3.film_id = fc3.film_id
         JOIN category AS c3 ON fc3.category_id = c3.category_id
-        WHERE c3.name = 'Horror') AS sub2 ON a.actor_id = sub2.actor_id
+        WHERE c3.name = 'Horror'
+) sub2 ON a.actor_id = sub2.actor_id
 WHERE sub2.actor_id IS NULL
 GROUP BY a.first_name,a.last_name
 ORDER BY last_name;
