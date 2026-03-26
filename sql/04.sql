72cfcbe013971d6dfd6343e580a57a94c60ac69b
dwells66 <dwells66@students.claremontmckenna.edu>
2026-03-26 01:03:19 -0700

    4!

--- sql/04.sql
+++ sql/04.sql
@@ -4,7 +4,7 @@
  * 2. but that have never appeared in any movie in the "Horror" category.
  */
 
-SELECT DISTINCT
+SELECT
     a.first_name,
     a.last_name
 FROM actor a
@@ -13,10 +13,11 @@ JOIN film_category AS fc ON fa.film_id = fc.film_id
 JOIN category AS c ON fc.category_id = c.category_id
 WHERE c.name = 'Children'
 AND a.actor_id NOT IN (
-        SELECT fa.actor_id
-        FROM film_actor fa
-        JOIN film_category AS fc ON fa.film_id = fc.film_id
-        JOIN category AS c ON fc.category_id = c.category_id
-        WHERE c.name = 'Horror'
+        SELECT fa2.actor_id
+        FROM film_actor fa2
+        JOIN film_category AS fc2 ON fa2.film_id = fc2.film_id
+        JOIN category AS c2 ON fc2.category_id = c2.category_id
+        WHERE c2.name = 'Horror'
 )
+GROUP BY a.first_name, a.last_name
 ORDER BY a.last_name,a.first_name;
