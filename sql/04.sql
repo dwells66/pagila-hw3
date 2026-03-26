8b4bf9fcd18c6460a895746f51f962881823f5a8
dwells66 <dwells66@students.claremontmckenna.edu>
2026-03-26 00:57:28 -0700

    4!

--- sql/04.sql
+++ sql/04.sql
@@ -8,19 +8,15 @@ SELECT DISTINCT
     a.first_name,
     a.last_name
 FROM actor a
-JOIN
-    (SELECT DISTINCT fa2.actor_id
-        FROM film_actor AS fa2
-        JOIN film_category AS fc2 ON fa2.film_id = fc2.film_id
-        JOIN category AS c2 ON fc2.category_id = c2.category_id
-        WHERE c2.name = 'Children'
-) AS sub1 ON a.actor_id = sub1.actor_id
-LEFT JOIN
-    (SELECT DISTINCT fa3.actor_id
-        FROM film_actor fa3
-        JOIN film_category AS fc3 ON fa3.film_id = fc3.film_id
-        JOIN category AS c3 ON fc3.category_id = c3.category_id
-        WHERE c3.name = 'Horror'
-) sub2 ON a.actor_id = sub2.actor_id
-WHERE sub2.actor_id IS NULL
+JOIN film_actor AS fa USING (actor_id)
+JOIN film_category AS fc ON fa.film_id = fc.film_id
+JOIN category AS c ON fc.category_id = c.category_id
+WHERE c.name = 'Children'
+AND a.actor_id NOT IN (
+        SELECT fa.actor_id
+        FROM film_actor fa
+        JOIN film_category AS fc ON fa.film_id = fc.film_id
+        JOIN category AS c ON fc.category_id = c.category_id
+        WHERE c.name = 'Horror'
+)
 ORDER BY a.last_name,a.first_name;
